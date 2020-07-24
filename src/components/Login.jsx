7347b49d27ed271204c15bfdc1e5723d7d74f557
Nathan Watkins Hoagland <watkins92@gmail.com>
2020-07-24 10:34:39 -0700

    Create & populate reducers, begin login implementation

---
+++ src/components/Login.jsx
@@ -0,0 +1,4 @@
+import React from 'react';
+import { login } from '../actions/Login';
+import { useSelector, useDispatch } from 'react-redux';
+import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
