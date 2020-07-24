02c42d2470ad6368b3cf750ef599f555c3084c5e
Nathan Watkins Hoagland <watkins92@gmail.com>
2020-07-24 13:39:32 -0700

    refactor login out of code until ready to connect it with firebase

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,4 +1,68 @@
-import React from 'react';
-import { login } from '../actions/Login';
-import { useSelector, useDispatch } from 'react-redux';
-import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
+// import React from "react";
+// import firebase from 'firebase/app';
+
+// function Login(){  
+//   function doSignUp(event) {
+//     event.preventDefault();
+//     const email = event.target.email.value;
+//     const password = event.target.password.value;
+//     firebase.auth().createUserWithEmailAndPassword(email, password).then(function(){
+//       console.log('Successfully signed up!');
+//     }).catch(function(error) {
+//       console.log(error.message);
+//     });
+//   }
+
+//   function doSignIn(event) {
+//     event.preventDefault();
+//     const email = event.target.signinEmail.value;
+//     const password = event.target.signinPassword.value;
+//     firebase.auth().signInWithEmailAndPassword(email, password).then(function(){
+//       console.log('Successfully signed in!');
+//     }).catch(function(error) {
+//       console.log(error.message);
+//     });
+//   }
+
+//   function doSignOut() {
+//     firebase.auth().signOut().then(function() {
+//       console.log('Successfully signed out!');
+//     }).catch(function(error) {
+//       console.log(error.message);
+//     });
+//   }
+
+//   return (
+//     <>
+//       <h1>Sign up</h1>
+//       <form onSubmit={doSignUp}>
+//         <input
+//           type='text'
+//           name='email'
+//           placeholder='email' />
+//         <input
+//           type='password'
+//           name='password'
+//           placeholder='Password' />
+//         <button type='submit'>Sign up</button>
+//       </form>
+
+//       <h1>Sign In</h1>
+//       <form onSubmit={doSignIn}>
+//         <input
+//           type='text'
+//           name='signinEmail'
+//           placeholder='email' />
+//         <input
+//           type='password'
+//           name='signinPassword'
+//           placeholder='Password' />
+//         <button type='submit'>Login</button>
+//       </form>
+//       <h1>Sign Out</h1>
+//       <button onClick={doSignOut}>Log Out</button>
+//     </>
+//   );
+// }
+
+// export default Login;
--- src/reducers/login-reducer.jsx
+++ src/reducers/login-reducer.jsx
@@ -1,38 +1,38 @@
-import * as c from '../actions/ActionTypes';
-import { Login } from '../actions/Login';
+// import * as c from '../actions/ActionTypes';
+// import { Login } from '../actions/Login';
 
-// I am unsure if all of this will click together as intended later
-// Might need to refactor, but is fine as is for now
+// // I am unsure if all of this will click together as intended later
+// // Might need to refactor, but is fine as is for now
 
-let initialState = {
-  isLoading: false,
-  userInfo: {},
-  error: null,
-  login: false,
-  authenticationFail: null
-}
+// let initialState = {
+//   isLoading: false,
+//   userInfo: {},
+//   error: null,
+//   login: false,
+//   authenticationFail: null
+// }
 
-export default (state = initialState, action) => {
-  switch (action.type) {
-    case c.REQUEST_LOGIN:
-      return Object.assign({}, state, {
-        isLoading: true
-      });
-    case c.GET_LOGIN_SUCCESS:
-      return Object.assign({}, state, {
-        isLoading: false,
-        login: true,
-        userInfo: action.userInfo
-      });
-    case c.GET_LOGIN_FAILURE:
-      return Object.assign({}, state, {
-        isLoading: false,
-        error: action.error,
-        login: false,
-        authenticationFail: true
-      });
-    default:
-      return state;
-  }
-};
+// export default (state = initialState, action) => {
+//   switch (action.type) {
+//     case c.REQUEST_LOGIN:
+//       return Object.assign({}, state, {
+//         isLoading: true
+//       });
+//     case c.GET_LOGIN_SUCCESS:
+//       return Object.assign({}, state, {
+//         isLoading: false,
+//         login: true,
+//         userInfo: action.userInfo
+//       });
+//     case c.GET_LOGIN_FAILURE:
+//       return Object.assign({}, state, {
+//         isLoading: false,
+//         error: action.error,
+//         login: false,
+//         authenticationFail: true
+//       });
+//     default:
+//       return state;
+//   }
+// };
 
