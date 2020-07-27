546587020f75c4f0c1adde8ca44328fd94aebc5d
Nathan Watkins Hoagland <watkins92@gmail.com>
2020-07-27 15:49:58 -0700

    Building out navbar

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,10 +4,12 @@ import React from 'react';
 // import Login from './Login';
 import Header from './Header';
 import StoryBoard from './StoryBoard';
+import Navbar from "./Navbar/Navbar"
 
 function App() {
   return (
     <Router>
+      <Navbar />
       <Header />
       <Switch>
         {/* <Route path ='/login'>
