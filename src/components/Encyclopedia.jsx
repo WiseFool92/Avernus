c22c9432a20ed87830e0d8690f2e58d2dd69f5ba
Nathan Watkins Hoagland <watkins92@gmail.com>
2020-07-29 10:52:48 -0700

    troubleshooting errors w/Object rendering

--- src/components/Encyclopedia.jsx
+++ src/components/Encyclopedia.jsx
@@ -1,16 +1,18 @@
-import React, { useState } from 'react';
+// import React, { useState } from 'react';
 // import DropdownButton from 'react-bootstrap/DropdownButton';
 // import Dropdown from 'react-bootstrap/Dropdown';
 // import { DropdownItem } from 'reactstrap';
 // import Dropdown from 'react';
+import Dropdown from 'react';
+import React, { Component } from 'react';
 
-function Encyclopedia() {
+// function Encyclopedia() {
 
-  return (
-    <>
-    <div>
-      <h1>Hi</h1>
-    </div>
+//   return (
+//     <>
+//     <div>
+//       <h1>Hi</h1>
+//     </div>
       {/* <DropdownButton
         alignRight
       >
@@ -31,8 +33,37 @@ function Encyclopedia() {
               <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
             </Dropdown.Menu>
           </Dropdown> */}
-    </>
+    {/* </>
   )
 };
 
-export default Encyclopedia;
+export default Encyclopedia; */}
+
+
+
+
+class Encyclopedia extends Component {
+  // constructor(props) {
+  //   super(props);
+  // }
+
+  render() {
+    return (
+      <>
+          <Dropdown>
+            <Dropdown.Toggle variant="success" id="dropdown-basic">
+              Dropdown Button
+            </Dropdown.Toggle>
+
+            <Dropdown.Menu>
+              <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
+              <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
+              <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
+            </Dropdown.Menu>
+          </Dropdown>
+      </>
+    );
+  }
+};
+
+export default Encyclopedia; 
