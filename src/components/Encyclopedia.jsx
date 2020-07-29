ee25befd1baa1f1ad6d0b9f0c7dbc0b60279a721
Nathan Watkins Hoagland <watkins92@gmail.com>
2020-07-29 15:22:54 -0700

    Cleaned up code & deleted excess

--- src/components/Encyclopedia.jsx
+++ src/components/Encyclopedia.jsx
@@ -1,9 +1,4 @@
 // import React, { useState } from 'react';
-// import DropdownButton from 'react-bootstrap/DropdownButton';
-// import Dropdown from 'react-bootstrap/Dropdown';
-// import { DropdownItem } from 'reactstrap';
-// import Dropdown from 'react';
-// import Dropdown from 'react';
 import React, { Component } from 'react';
 import Dropdown from './Dropdown/Dropdown';
 import items from './Dropdown/Items'
@@ -19,57 +14,8 @@ function Encyclopedia() {
         <Dropdown title='Explore' items={items} multiSelect />
       </div>
     </div>
-      {/* <DropdownButton
-        alignRight
-      >
-        <DropdownItem eventKey = 'Regions'>Regions</DropdownItem>
-        <DropdownItem eventKey = 'Beastiary'>Beastiary</DropdownItem>
-      </DropdownButton>
-       
-
-
-           <Dropdown>
-            <Dropdown.Toggle variant="success" id="dropdown-basic">
-              Dropdown Button
-            </Dropdown.Toggle>
-
-            <Dropdown.Menu>
-              <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
-              <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
-              <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
-            </Dropdown.Menu>
-          </Dropdown>  */}
      </>
   )
 };
 
 export default Encyclopedia;
-
-
-
-
-// class Encyclopedia extends Component {
-//   // constructor(props) {
-//   //   super(props);
-//   // }
-
-//   render() {
-//     return (
-//       <>
-//           <Dropdown>
-//             <Dropdown.Toggle variant="success" id="dropdown-basic">
-//               Dropdown Button
-//             </Dropdown.Toggle>
-
-//             <Dropdown.Menu>
-//               <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
-//               <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
-//               <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
-//             </Dropdown.Menu>
-//           </Dropdown>
-//       </>
-//     );
-//   }
-// };
-
-// export default Encyclopedia; 
