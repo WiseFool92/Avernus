3fb6067cfde1ae34a84d278701eb064b69473684
Nathan Watkins Hoagland <watkins92@gmail.com>
2020-07-29 16:05:38 -0700

    Clean & align encyclopedia page

--- src/components/Encyclopedia.jsx
+++ src/components/Encyclopedia.jsx
@@ -6,10 +6,16 @@ import './App.scss';
 
 function Encyclopedia() {
 
+  const align = {
+    textAlign: 'center',
+    padding: '1rem',
+    margin: '1rem',
+  }
+
   return (
     <>
     <div>
-      <h1>Hi</h1>
+      <h1 style={align}></h1>
       <div className='container'>
         <Dropdown title='Explore' items={items} multiSelect />
       </div>
