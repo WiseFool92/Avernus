3f73a2684fdfe00695cfef57d593a62ddc53bc9a
Nathan Watkins Hoagland <watkins92@gmail.com>
2020-07-29 12:40:32 -0700

    feeling overwhelmed

--- src/components/Encyclopedia.jsx
+++ src/components/Encyclopedia.jsx
@@ -6,23 +6,23 @@
 import Dropdown from 'react';
 import React, { Component } from 'react';
 
-// function Encyclopedia() {
+function Encyclopedia() {
 
-//   return (
-//     <>
-//     <div>
-//       <h1>Hi</h1>
-//     </div>
-      {/* <DropdownButton
+  return (
+    <>
+    <div>
+      <h1>Hi</h1>
+    </div>
+      <DropdownButton
         alignRight
       >
         <DropdownItem eventKey = 'Regions'>Regions</DropdownItem>
         <DropdownItem eventKey = 'Beastiary'>Beastiary</DropdownItem>
       </DropdownButton>
-       */}
+       
 
 
-          {/* <Dropdown>
+           <Dropdown>
             <Dropdown.Toggle variant="success" id="dropdown-basic">
               Dropdown Button
             </Dropdown.Toggle>
@@ -32,38 +32,38 @@ import React, { Component } from 'react';
               <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
               <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
             </Dropdown.Menu>
-          </Dropdown> */}
-    {/* </>
+          </Dropdown> 
+     </>
   )
 };
 
-export default Encyclopedia; */}
+export default Encyclopedia;
 
 
 
 
-class Encyclopedia extends Component {
-  // constructor(props) {
-  //   super(props);
-  // }
+// class Encyclopedia extends Component {
+//   // constructor(props) {
+//   //   super(props);
+//   // }
 
-  render() {
-    return (
-      <>
-          <Dropdown>
-            <Dropdown.Toggle variant="success" id="dropdown-basic">
-              Dropdown Button
-            </Dropdown.Toggle>
+//   render() {
+//     return (
+//       <>
+//           <Dropdown>
+//             <Dropdown.Toggle variant="success" id="dropdown-basic">
+//               Dropdown Button
+//             </Dropdown.Toggle>
 
-            <Dropdown.Menu>
-              <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
-              <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
-              <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
-            </Dropdown.Menu>
-          </Dropdown>
-      </>
-    );
-  }
-};
+//             <Dropdown.Menu>
+//               <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
+//               <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
+//               <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
+//             </Dropdown.Menu>
+//           </Dropdown>
+//       </>
+//     );
+//   }
+// };
 
-export default Encyclopedia; 
+// export default Encyclopedia; 
