91e8957df7a306c73307288c8ba25d47fedb47ca
Nathan Watkins Hoagland <watkins92@gmail.com>
2020-09-14 22:44:10 -0700

    Implemented the map of the Western Gallery

--- src/components/Encyclopedia.jsx
+++ src/components/Encyclopedia.jsx
@@ -1,8 +1,9 @@
 // import React, { useState } from 'react';
 import React, { Component } from 'react';
 import Dropdown from './Dropdown/Dropdown';
-import items from './Dropdown/Items'
-import './Dropdown/App.scss'
+import items from './Dropdown/Items';
+import './Dropdown/App.scss';
+import westernGallery from './Img/westernGallery.jpg'
 
 function Encyclopedia() {
 
@@ -12,12 +13,30 @@ function Encyclopedia() {
     margin: '1rem',
   }
 
+  const westernGalleryStyles = {
+    borderRadius: '5px',
+    boxShadow: '5px 5px 8px 5px rgba(0, 0, 0, 0.449)',
+    marginTop: '3rem',
+    marginBottom: '3rem',
+    display: 'block',
+    maxWidth: '100vw',
+    maxHeight: '100vh',
+    marginLeft: 'auto',
+    marginRight: 'auto',
+    width: 'auto',
+    height: 'auto',
+    
+  }
+
   return (
     <>
     <div>
       <h1 style={align}></h1>
       <div className='container'>
         <Dropdown title='Explore' items={items} multiSelect />
+      </div>
+      <div>
+        <img src={westernGallery} style={westernGalleryStyles} alt='Eastern Gallery' ></img>
       </div>
     </div>
      </>
