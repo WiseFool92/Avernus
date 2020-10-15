51d97fd942ccee561ceb5e9fc5b6b4984b2543f2
Nathan Watkins Hoagland <watkins92@gmail.com>
2020-10-15 13:09:52 -0700

    Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -22,11 +22,26 @@ export default function Footer() {
     <>
       <div style={footer}>
         <div style={{marginTop: '1rem', paddingTop: '2rem'}}>
-          <a href="https://www.facebook.com/Grant-Us-Funding-104687287971432" className="fab fa-facebook fa-2x" style={fabLinkStyle}> </a>
-          <a href="https://twitter.com/GrantUsFunding" className="fab fa-twitter fa-2x" style={fabLinkStyle}> </a>
-          {/* <a href="/tbd" className="fab fa-instagram fa-2x" style={fabLinkStyle}> </a> */}
-          <a href="https://www.linkedin.com/in/grantusfunding/" className="fab fa-linkedin fa-2x" style={fabLinkStyle}> </a>
-          <a href="https://www.youtube.com/channel/UCWO63zfjdC941HVwvTK_kFw?guided_help_flow=3" className="fab fa-youtube fa-2x" style={fabLinkStyle}> </a>
+          <a href="https://github.com/WiseFool92" className="fab fa-github fa-2x" style={fabLinkStyle}> </a>
+          <a href="https://www.instagram.com/mr.supernova/" className="fab fa-instagram fa-2x" style={fabLinkStyle}> </a>
+          <a href="https://www.linkedin.com/in/nathan-watkins-hoagland/" className="fab fa-linkedin fa-2x" style={fabLinkStyle}> </a>
+          <a href="https://www.youtube.com/channel/UCo4USBh5QCeNpv9nvDYoX8Q?view_as=subscriber" className="fab fa-youtube fa-2x" style={fabLinkStyle}> </a>
+        </div>
+        <div className='container'>
+          <div className='row'>
+            <ul style={{listStyle: 'none'}}>
+              <li>
+                <Link to='/contact'><span style={footerLinks}>Contact</span></Link>
+              </li>
+            </ul>
+          </div>
+          <div>
+            <p style={{background: '#959595', marginBottom: '0rem', marginTop: '-2rem'}}>
+              &copy; 2020-{new Date().getFullYear()}, Avernus
+              <br/>
+              All Right Reserved | <Link to='/terms'><span style={links}>Terms of Service</span></Link> | <Link to='/privacypolicy'><span style={links}>Privacy Policy</span></Link>
+            </p>
+          </div>
         </div>
       </div>
     </>
